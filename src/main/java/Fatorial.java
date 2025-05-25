1bfa211f7ace187075507e914b5e38cf7e961356
Gabriel Felipe Guarnieri <124581091+oguarni@users.noreply.github.com>
2025-05-25 02:52:39 -0300

    Update Fatorial.java

--- src/main/java/Fatorial.java
+++ src/main/java/Fatorial.java
@@ -11,25 +11,24 @@ public class Fatorial {
     }
 }
 
-/*
 a) GRAFO DE FLUXO DE CONTROLE (CFG):
 
 ```mermaid
 Graph TD
-    A[INÍCIO] --&gt; B[Nó 1: resultado = 1];
-    B --&gt; C{Nó 2: n &lt; 0?};
-    C -- SIM --&gt; D[Nó 3];
-    C -- NÃO --&gt; E{Nó 4: i = 1; i &lt;= n?};
-    D --&gt; G(Nó 6: return resultado);
-    E -- SIM --&gt; F[Nó 5];
-    E -- NÃO --&gt; G;
-    F --&gt; E; /* Loop */
-    G --&gt; H[FIM];
+    A[INÍCIO] --> B[Nó 1: resultado = 1];
+    B --> C{Nó 2: n < 0?};
+    C -- SIM --> D[Nó 3];
+    C -- NÃO --> E{Nó 4: i = 1; i <= n?};
+    D --> G(Nó 6: return resultado);
+    E -- SIM --> F[Nó 5];
+    E -- NÃO --> G;
+    F --> E; /* Loop */
+    G --> H[FIM];
 
     subgraph Caminhos Independentes
-        P1(Caminho 1: 1 -&gt; 2 -&gt; 3 -&gt; 6 (n negativo));
-        P2(Caminho 2: 1 -&gt; 2 -&gt; 4 -&gt; 6 (n = 0, sem entrar no loop));
-        P3(Caminho 3: 1 -&gt; 2 -&gt; 4 -&gt; 5 -&gt; 4 -&gt; 6 (n &gt; 0, entrando no loop uma vez));
+        P1(Caminho 1: 1 -> 2 -> 3 -> 6 (n negativo));
+        P2(Caminho 2: 1 -> 2 -> 4 -> 6 (n = 0, sem entrar no loop));
+        P3(Caminho 3: 1 -> 2 -> 4 -> 5 -> 4 -> 6 (n > 0, entrando no loop uma vez));
     end
 
 b) CAMINHOS INDEPENDENTES:
